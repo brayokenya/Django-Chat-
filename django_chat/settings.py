362bba19e6ab6bd4fdfa02feb70017d8cee2a26c
brayokenya <kiirubrian21@gmail.com>
2023-10-25 23:26:19 +0300

    added values to .env

--- django_chat/settings.py
+++ django_chat/settings.py
@@ -19,8 +19,6 @@ import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
